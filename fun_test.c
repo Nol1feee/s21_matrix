9be734484403da959edbf69b01b3628e3254c37a
Snape Burl <snapebur@at-l6.msk.21-school.ru>
2023-03-11 21:47:33 +0300

    under mult matrix

--- fun_test.c
+++ fun_test.c
@@ -5,15 +5,32 @@ int main() {
   s21_create_matrix(5, 5, &kek);
   matrix_t k = {0};
   s21_create_matrix(5, 5, &k);
-  printf("\n%d\n", s21_eq_matrix(&kek, &k));
-//  		for(int i = 0; i < 5; i++) {
-//  				for(int j = 0; j < 5; j++) {
-//  						printf("%.0f ",
-//  kek.matrix[i][j]);
-//  				}
-//  				printf("\n");
-//  		}
-//printf("\n");
+  matrix_t wow = {0};
+  s21_create_matrix(5, 5, &wow);
+    s21_eq_matrix(&kek, &k);
+     for(int i = 0; i < 5; i++) {
+         for(int j = 0; j < 5; j++) {
+            k.matrix[i][j] = i * j;
+        }
+    }
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+   printf("%.0f ", k.matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n\n\n");
+  s21_mult_number( &k, 2, &wow);
+
+  		for(int i = 0; i < 5; i++) {
+  				for(int j = 0; j < 5; j++) {
+  						printf("%.0f ", wow.matrix[i][j]);
+  				}
+  				printf("\n");
+  		}
+
+printf("\n");
 //		for(int i = 0; i < 5; i++) {
 //				for(int j = 0; j < 5; j++) {
 //						printf("%.0f ",
